f67c7277996c409c357f526316fbbba24d209e89
Valentino <valentinoclevy@gmail.com>
2024-05-07 17:33:57 -0300

    Terminé Alfajores

--- Alfajores.hs
+++ Alfajores.hs
@@ -1,4 +1,5 @@
 import Text.Show.Functions
+import Data.List
  
 data Alfajor = UnAlfajor {
    relleno :: [String],
@@ -56,4 +57,82 @@ hacerPremium unAlfajor
     | esPotable unAlfajor = agregarCapa [last (relleno unAlfajor)] (renombrarAlfajor unAlfajor (nombre unAlfajor ++ " Premium"))
     | otherwise = unAlfajor
 
--- agregarCapa [last (relleno unAlfajor)] unAlfajor
+hacerPremiumGrado :: Int -> Alfajor -> Alfajor
+hacerPremiumGrado 0 unAlfajor = unAlfajor
+hacerPremiumGrado grado unAlfajor
+    | esPotable unAlfajor = hacerPremiumGrado (grado - 1) (hacerPremium unAlfajor)
+    | otherwise = unAlfajor
+
+{- hacerPremiumGrado :: Alfajor -> Int -> Alfajor
+hacerPremiumGrado unAlfajor grado
+    | grado <= 0 = unAlfajor
+    | esPotable unAlfajor = foldl (\alfajor _ -> hacerPremium alfajor) unAlfajor [1..grado]
+    | otherwise = unAlfajor -} -- otra forma con fold y lamdba
+
+jorgitito::Alfajor
+jorgitito = abaratarAlfajor.modificarNombre "Jorgitito" $ jorgito
+
+jorgelin::Alfajor
+jorgelin = agregarCapa ["Dulce de leche"].modificarNombre "Jorgelin" $ jorgito
+
+capitanDelEspacioCostaACosta :: Alfajor
+capitanDelEspacioCostaACosta = modificarNombre "Capitan del espacio costa a costa" (hacerPremiumGrado 4.abaratarAlfajor $ capitanDelEspacio) 
+
+data Cliente = UnCliente {
+    nombreCliente::String,
+    dinero::Int,
+    criterio::Criterio,
+    alfajoresComprados::[Alfajor]
+} deriving Show
+
+type Criterio = Alfajor->Bool
+
+nombreCapitanDelEspacio:: Criterio
+nombreCapitanDelEspacio unAlfajor = isInfixOf "Capitan del espacio" (nombre unAlfajor)
+
+emi::Cliente
+emi = UnCliente "Emi" 120 nombreCapitanDelEspacio []
+
+criterioTomi::Criterio
+criterioTomi unAlfajor = pretencioso unAlfajor && dulcero unAlfajor
+
+pretencioso :: Criterio
+pretencioso unAlfajor =  isInfixOf "Premium" (nombre unAlfajor)
+
+dulcero :: Criterio
+dulcero unAlfajor =  coeficienteDeDulzor unAlfajor > 0.15
+
+tomi::Cliente
+tomi = UnCliente "tomi" 100 criterioTomi []
+
+dante::Cliente
+dante = UnCliente "dante" 200 criterioDante []
+
+criterioDante::Criterio
+criterioDante unAlfajor = anti "Dulce de leche" unAlfajor && extranio unAlfajor
+
+extranio::Criterio
+extranio unAlfajor = not.esPotable $ unAlfajor
+
+anti:: String -> Criterio
+anti unRelleno unAlfajor = not.elem unRelleno $ (relleno unAlfajor)
+
+juan::Cliente
+juan = UnCliente "juan" 500 criterioJuan []
+
+criterioJuan::Criterio
+criterioJuan unAlfajor = criterioTomi unAlfajor && isInfixOf "jorgito" (nombre unAlfajor) && anti "Mousse" unAlfajor
+
+alfajores::[Alfajor]
+alfajores = [jorgito, havanna, capitanDelEspacio, jorgelin, hacerPremium havanna]
+
+cualesLeGustan :: Cliente -> [Alfajor] -> [Alfajor]
+cualesLeGustan unCliente alfajores = filter (criterio unCliente) alfajores
+
+comprarAlfajor :: Cliente -> Alfajor -> Cliente
+comprarAlfajor unCliente unAlfajor 
+    | dinero unCliente >= precioDeAlfajor unAlfajor = unCliente { alfajoresComprados = unAlfajor : alfajoresComprados unCliente }
+    | otherwise = unCliente
+
+comprarLosQueLeGustan :: Cliente -> [Alfajor] -> Cliente
+comprarLosQueLeGustan unCliente alfajores = foldl comprarAlfajor unCliente alfajores
