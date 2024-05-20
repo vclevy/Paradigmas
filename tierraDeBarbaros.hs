abfc95acaf3be619a04cd3f33408a0fd20c0fc8d
delfisartorio <msartorio@frba.utn.edu.ar>
2024-05-20 16:56:03 -0300

    Hasta el punto 4

---
+++ tierraDeBarbaros.hs
@@ -0,0 +1,94 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Move filter" #-}
+{-# HLINT ignore "Eta reduce" #-}
+{-# HLINT ignore "Use infix" #-}
+import Data.Char (toUpper,isUpper)
+import Text.Show.Functions
+import Data.List (intersect)
+
+data Barbaro = Barbaro{
+    nombre :: String,
+    fuerza :: Int,
+    habilidades :: [String],
+    objetos :: [Objetos]
+}deriving(Show)
+
+type Objetos = Barbaro->Barbaro
+dave :: Barbaro
+dave = Barbaro "Dave" 100 ["tejer","escribirPoesia"] [ardilla, varitasDefectuosas]
+
+--PUNTO 1
+
+espada :: Int -> Objetos
+espada kilos unBarbaro = Barbaro {fuerza = fuerza unBarbaro + 2*kilos}
+
+amuletosMisticos :: String -> Objetos
+amuletosMisticos habilidad unBarbaro = Barbaro{habilidades = habilidad : habilidades unBarbaro}
+
+varitasDefectuosas :: Objetos
+varitasDefectuosas unBarbaro = (amuletosMisticos "hacer magia" unBarbaro){objetos = []}
+
+ardilla :: Objetos
+ardilla unBarbaro = unBarbaro
+
+cuerda :: Objetos -> Objetos -> Objetos
+cuerda unObjeto otroObjeto = unObjeto.otroObjeto
+
+--PUNTO 2
+
+megafono :: Objetos
+megafono = undefined
+
+megafonoBarbarico :: Objetos
+megafonoBarbarico = cuerda megafono ardilla
+
+--PUNTO 3
+
+invasionDeSuciosDuendes :: Evento
+invasionDeSuciosDuendes = elem "Escribir Poesía Atroz".habilidades
+
+cremalleraDelTiempo :: Evento
+cremalleraDelTiempo unBarbaro = nombre unBarbaro == "Faffy" || nombre unBarbaro == "Astro"
+
+totalLength :: [String] -> Int
+totalLength list = length (concat list) --Cuando desarrolle megafono quizas se puede reemplazar
+
+saqueo :: Evento
+saqueo unBarbaro = fuerza unBarbaro>80 && elem "robar" (habilidades unBarbaro)
+
+gritoDeGuerra :: Evento
+gritoDeGuerra unBarbaro = 4* (length.objetos) unBarbaro == (totalLength.habilidades) unBarbaro
+
+caligrafia :: Evento
+caligrafia unBarbaro = masDe3Vocales unBarbaro && comienzanConMayuscula unBarbaro
+
+masDe3Vocales :: Evento
+masDe3Vocales unBarbaro = ((3<).length.filter (`elem` vocales)) (concat (habilidades unBarbaro))
+vocales :: [Char]
+vocales = "aáAÁeéEÉiíÍIoOóÓuúÚU"
+
+comienzanConMayuscula :: Evento
+comienzanConMayuscula unBarbaro = all (isUpper . head) (habilidades unBarbaro)
+
+type Evento = Barbaro -> Bool
+type Aventura = [Evento]
+
+ritualDeFechorias :: Evento
+ritualDeFechorias unBarbaro = saqueo unBarbaro || gritoDeGuerra unBarbaro || caligrafia unBarbaro
+
+aventuraEscribirA4dedos :: [Evento]
+aventuraEscribirA4dedos = [invasionDeSuciosDuendes, cremalleraDelTiempo]
+
+aventuraTodoPoderoso :: [Evento]
+aventuraTodoPoderoso = [ritualDeFechorias]
+
+sobrevivientes :: Aventura -> [Barbaro] -> [Barbaro]
+sobrevivientes unaAventura unGrupo = filter (sobreviveAventura unaAventura) unGrupo
+
+sobreviveAventura :: Aventura -> Barbaro -> Bool
+sobreviveAventura aventura barbaro = all (\evento -> evento barbaro) aventura
+
+--PUNTO 4
+
+sinRepetidos :: [String] -> [String]
+sinRepetidos lista = intersect lista lista 
