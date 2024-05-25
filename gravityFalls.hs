30eeb9f23cbfee538c9f3391a1cd56ffb1a1108d
delfisartorio <msartorio@frba.utn.edu.ar>
2024-05-25 10:50:55 -0300

    hasta el pto de zipwhitif que no se entiende

--- gravityFalls.hs
+++ gravityFalls.hs
@@ -1,5 +1,4 @@
 import Text.Show.Functions
-import GHC.ResponseFile (escapeArgs)
 
 data Persona = Persona{
     edad :: Int,
@@ -9,29 +8,55 @@ data Persona = Persona{
 
 data Criatura = Criatura{
     peligrosidad:: Int,
-    deshacerseDe :: String --por ahora
+    deshacerseDe :: Persona->Bool --por ahora
 } deriving(Show)
 
+type Condicion = Persona -> Bool
+
 siempredetras:: Criatura
-siempredetras = Criatura 0 ""
+siempredetras = Criatura 0 nadaQueHacer
+
+nadaQueHacer:: Persona -> Bool
+nadaQueHacer _ = False
 
 peligrosidadGnomos :: Int -> Int
 peligrosidadGnomos cantidad = 2^cantidad
 
-gnomos ::Int -> Criatura
-gnomos cantidad = Criatura{peligrosidad= peligrosidadGnomos cantidad,deshacerseDe= "soplador de hojas"}
+poseeElemento :: String -> Persona -> Bool
+poseeElemento elemento persona = elemento `elem` items persona
 
-fantasma :: Int -> String ->Criatura
+gnomos ::Int -> Criatura
+gnomos cantidad = Criatura{peligrosidad= peligrosidadGnomos cantidad,deshacerseDe= poseeElemento "soplador de hojas"}
+
+fantasma :: Int -> Condicion ->Criatura
 fantasma categoria condicion = Criatura{peligrosidad = 20*categoria,deshacerseDe = condicion}
 
 modificarExperiencia :: (Int->Int) -> Persona -> Persona
 modificarExperiencia modificador unaPersona = unaPersona{experiencia= modificador (experiencia unaPersona)}
 
 tieneLoQueSeNecesita :: Criatura -> Persona -> Bool
-tieneLoQueSeNecesita unaCriatura unaPersona = deshacerseDe unaCriatura `elem` items unaPersona
+tieneLoQueSeNecesita  = deshacerseDe   
 
 enfrenarCriatura :: Persona-> Criatura -> Persona
 enfrenarCriatura unaPersona unaCriatura 
     | tieneLoQueSeNecesita unaCriatura unaPersona = modificarExperiencia (+peligrosidad unaCriatura) unaPersona
     | otherwise = modificarExperiencia (+1) unaPersona
+
+enfrentosSucesivos :: [Criatura] -> Persona -> Persona
+enfrentosSucesivos unasCriaturas unaPersona = foldl enfrenarCriatura unaPersona unasCriaturas
+
+gnomos10 :: Criatura
+gnomos10 = gnomos 10
+
+condicionFantasma3 :: Persona -> Bool
+condicionFantasma3 unaPersona = poseeElemento "disfraz de oveja" unaPersona && 13>edad unaPersona
+
+fantasma3 :: Criatura
+fantasma3 = fantasma 3 condicionFantasma3 
+
+fantasma1 :: Criatura
+fantasma1 = fantasma 1 ((10<).experiencia)
+
+persona1 ::Persona
+persona1 = Persona 10 ["caca", "disfraz de oveja"] 0
 
